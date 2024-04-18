ba5dfa07c3c231bb14a14fd6e81a99755bacf520
giosciafra <francescogioscia8@gmail.com>
2024-04-18 18:25:25 +0200

    feat: column table

--- database/migrations/2024_04_18_153659_create_projects_table.php
+++ database/migrations/2024_04_18_153659_create_projects_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->text('img')->nullable();
+            $table->text('used_technologies');
+            $table->text('url');
             $table->timestamps();
         });
     }
